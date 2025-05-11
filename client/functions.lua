0e62cba493ce9a33bc54dc8254ac27f4effaf12a
Quasar <fernando.quasarstore@gmail.com>
2025-05-11 18:01:24 +0200

    Edited

--- client/functions.lua
+++ client/functions.lua
@@ -33,7 +33,7 @@ function QBCore.Functions.GetCoords(entity)
 end
 
 function QBCore.Functions.HasItem(items, amount)
-    return exports['qb-inventory']:HasItem(items, amount)
+    return exports['qs-advancedinventory']:HasItem(items, amount)
 end
 
 ---@param entity number - The entity to look at
